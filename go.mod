40e1785564b989df7a26c62c558855335525c8bd
Saul Maldonado <samaldonado4@gmail.com>
2021-04-11 04:22:42 -0500

    refactor: Move ping flows into functions

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/saulmaldonado/agones-minecraft
 
 go 1.16
 
-require github.com/Raqbit/mc-pinger v0.1.1
+require (
+	agones.dev/agones v1.13.0 // indirect
+	github.com/Raqbit/mc-pinger v0.1.1
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+	google.golang.org/genproto v0.0.0-20210406143921-e86de6bf7a46 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
+)
--- main.go
+++ main.go
@@ -24,41 +24,74 @@ func init() {
 	flag.DurationVar(&timeout, "timeout", interval, "Ping timeout")
 
 	flag.Parse()
-
 	logger = log.New(os.Stdout, "[agones-mc-monitor] ", log.Ltime|log.Ldate)
 }
 
 func main() {
-	var pinger mcpinger.Pinger
+	// Ping server until startup
+	info, err := pingUntilSuccessful(retry)
 
+	// Exit in case of unsuccessful startup
+	if err != nil {
+		logger.Println(err)
+		logger.Fatal("Fatal Mincraft server. Exiting...")
+	}
+
+	logger.Println(info)
+
+	// Pause before starting next ping cycle
+	time.Sleep(interval)
+
+	// Ping infinitely or until after a series of unsuccessful pings
+	err = pingUntilFatal(retry)
+
+	// Exit in case of fatal server
+	if err != nil {
+		logger.Println(err)
+		logger.Fatal("Fatal Mincraft server. Exiting...")
+	}
+}
+
+// Creates a pinger with Context containing timeout
+func ping() (*mcpinger.ServerInfo, error) {
+	pinger := mcpinger.NewTimed(host, uint16(port), timeout)
+	return pinger.Ping()
+}
+
+// Creates pings the server on an interval until the first sucessful ping which then will return the server info response
+// After a series of failed retries an error will be returned indicating a fatal server
+func pingUntilSuccessful(retry uint) (*mcpinger.ServerInfo, error) {
+	info, err := ping()
+
+	for err != nil && retry > 0 {
+		logger.Println(err)
+		logger.Printf("Unsuccessful ping. retrying in %v...", interval)
+		time.Sleep(interval)
+
+		info, err = ping()
+		retry--
+	}
+
+	// Server will respond with an empty JSON object if its starting up
+	// Pings will continue to happen until the server returns the expected response
+	for info.Players.Max == 0 {
+		logger.Println("Minecraft server starting up...")
+		time.Sleep(interval)
+		info, err = ping()
+	}
+
+	return info, err
+}
+
+// Pings the server infinitely or the server fails to reposnd after a series of retries
+func pingUntilFatal(retry uint) error {
 	for {
-		pinger = mcpinger.NewTimed(host, uint16(port), timeout)
-		info, err := pinger.Ping()
+		info, err := pingUntilSuccessful(retry)
 
 		if err != nil {
-			retry := retry
-
-			for retry > 0 {
-				logger.Println(err)
-				logger.Printf("Unsuccessful ping. retrying in %v...", interval)
-				time.Sleep(interval)
-
-				pinger = mcpinger.NewTimed(host, uint16(port), timeout)
-				info, err = pinger.Ping()
-				if err != nil {
-					retry--
-				} else {
-					break
-				}
-			}
-
-			if err != nil {
-				logger.Println(err)
-				logger.Fatalf("Pings unsuccessful. Exiting...")
-			}
+			return err
 		}
 
 		logger.Println(info)
-		time.Sleep(interval)
 	}
 }
