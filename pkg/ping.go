5f677a63c5e7aed9547cfaf4dc13a23ccbc26b22
Saul Maldonado <samaldonado4@gmail.com>
2021-04-12 05:08:16 -0500

    feat: Add ping package
    Ping package is a library for pinging a minecraft server and signaling
    a local Agones server

---
+++ pkg/ping.go
@@ -0,0 +1,99 @@
+package ping
+
+import (
+	"time"
+
+	sdk "agones.dev/agones/sdks/go"
+	mcpinger "github.com/Raqbit/mc-pinger"
+)
+
+type AgonesPinger struct {
+	host    string
+	port    uint16
+	timeout time.Duration
+	Sdk     *sdk.SDK
+}
+
+// Creates a new AgonesPinger with that will ping the minecraft server at the given host and on the given port.
+// Also initializes a connection with the local Agones server on localhost port 9357.
+// Blocks until connection and handshake is made. Timesout and returns an error after 30 seconds
+func New(host string, port uint16) (*AgonesPinger, error) {
+	sdk, err := sdk.NewSDK()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &AgonesPinger{host: host, port: port, Sdk: sdk}, nil
+}
+
+// Creates a new AgonesPinger with that will ping the minecraft server at the given host and on the given port.
+// Ping will timeout after the give timeout duration.
+// Also initializes a connection with the local Agones server on localhost port 9357.
+// Blocks until connection and handshake is made. Timesout and returns an error after 30 seconds
+func NewTimed(host string, port uint16, timeout time.Duration) (*AgonesPinger, error) {
+	sdk, err := sdk.NewSDK()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &AgonesPinger{host, port, timeout, sdk}, nil
+}
+
+// Pings the minecraft server and sends Health() signal to the local Agones server on localhost port 9357
+// Returns an error if the ping is unsuccessful
+func (p *AgonesPinger) HealthPing() error {
+	_, err := p.ping()
+
+	if err != nil {
+		return err
+	}
+
+	return p.Sdk.Health()
+}
+
+// Pings the minecraft server and sends Ready() signal to the local Agones server on localhost port 9357
+// Returns an error if the ping is unsuccessful
+func (p *AgonesPinger) ReadyPing() error {
+	_, err := p.ping()
+
+	if err != nil {
+		return err
+	}
+
+	return p.Sdk.Ready()
+}
+
+// Pings the minecraft server and sends Health() signal to the local Agones server on localhost port 9357
+// Returns an error if the ping is unsuccessful or timeouts
+func (p *AgonesPinger) HealthPingWithTimeout() error {
+	_, err := p.pingWithTimeout()
+
+	if err != nil {
+		return err
+	}
+
+	err = p.Sdk.Health()
+	return err
+}
+
+// Pings the minecraft server and sends Ready() signal to the local Agones server on localhost port 9357
+// Returns an error if the ping is unsuccessful or timeouts
+func (p *AgonesPinger) ReadyPingWithTimeout() error {
+	_, err := p.pingWithTimeout()
+
+	if err != nil {
+		return err
+	}
+
+	return p.Sdk.Ready()
+}
+
+func (p *AgonesPinger) ping() (*mcpinger.ServerInfo, error) {
+	return mcpinger.New(p.host, p.port).Ping()
+}
+
+func (p *AgonesPinger) pingWithTimeout() (*mcpinger.ServerInfo, error) {
+	return mcpinger.NewTimed(p.host, p.port, p.timeout).Ping()
+}
